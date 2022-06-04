65caa595e7710041fe5aca2693c74d20c1c2e854
jhood44 <jhood4@gmail.com>
2022-06-04 02:06:02 -0400

    basic pill stuff for igredients

---
+++ MellysUndergroundCuisine/wwwroot/js/ingredientPills.js
@@ -0,0 +1,12 @@
+﻿$('#ingredientInput').on('keyup', function (e) {
+  if (e.which == 188) {
+    let current = $('#ingredientInput').val();
+    current = current.replace(/,/g, '');
+    let list = document.getElementById('ingredientList');
+    let li = document.createElement("li");
+    li.classList.add("ingredientInputItem", "badge", "rounded-pill", "ingredientPills");
+    li.appendChild(document.createTextNode(current));
+    list.prepend(li);
+    $(this).val("");
+  }
+})
