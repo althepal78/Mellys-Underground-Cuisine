ab223385f6a82a71a69b1a26ef369a261333a5e9
Albert Cardona II <bigdaddycardona@live.com>
2022-06-08 23:30:19 -0500

    more shit :)

---
+++ MellysUndergroundCuisine/Models/ViewModels/AddIngredientVM.cs
@@ -0,0 +1,10 @@
+﻿namespace MellysUndergroundCuisine.Models.ViewModels
+{
+    public class AddIngredientVM
+    {
+        public Guid ID { get; set; }
+        public Guid DishId { get; set; }
+        public string Name { get; set; }
+        public string NormalizeName { get; set; }
+    }
+}
